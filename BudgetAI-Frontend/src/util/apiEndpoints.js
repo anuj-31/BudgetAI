d09b1d2966437045d501cfaa89949e11deab1337
Anuj Kumar <kumaranuj98208@gmail.com>
2026-09-10 23:11:22 +0530

    changed the env for base url or back url

--- BudgetAI-Frontend/src/util/apiEndpoints.js
+++ BudgetAI-Frontend/src/util/apiEndpoints.js
@@ -1,4 +1,5 @@
-export const BASE_URL = "http://localhost:8080/api/v1.0";
+export const BASE_URL = import.meta.env.VITE_API_BASE_URL;
+// console.log("BASE_URL", BASE_URL);
 
 export const API_ENDPOINTS = {
     LOGIN: "/login",
